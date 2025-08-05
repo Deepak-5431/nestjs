f01af1f9e13b3b0561007c9d3ac3faf7894d5837
Deepak-5431 <deepakchand540@gmail.com>
2025-08-05 16:33:08 +0530

    nothing major change added swagger updated main

--- server/src/app.service.ts
+++ server/src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World! from server side nestjs';
+    return 'Hello World! ok from server side nestjs';
   }
 }
--- server/src/main.ts
+++ server/src/main.ts
@@ -1,13 +1,22 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder,SwaggerModule } from '@nestjs/swagger';
 import { HttpExceptionFilter } from './common/filters/http-exception.filter';
 import { TransformInterceptor } from './common/interceptors/transform.interceptor';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.useGlobalPipes(new ValidationPipe());
+  app.useGlobalPipes(new ValidationPipe());  
   app.useGlobalFilters(new HttpExceptionFilter());
   app.useGlobalInterceptors(new TransformInterceptor());
+  const config = new DocumentBuilder().setTitle('dodo api').setDescription("description api for managing users and their todos")
+  .setVersion('1.0')
+  .addTag('todos','endpoints related to managing todos')
+  .addTag('auth','endpoints for user authentication')
+  .addBearerAuth()
+  .build();
+  const document  = SwaggerModule.createDocument(app,config);
+  SwaggerModule.setup('api-docs',app,document);
   await app.listen(process.env.PORT ?? 5000);
 }
 bootstrap();
